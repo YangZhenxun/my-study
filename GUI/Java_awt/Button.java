06aed94a7bf2fdd6e9dbd75710c43792b7b82ee5
YangZhenxun <2210067895@qq.com>
2022-07-18 12:58:30 +0800

    I make a Button on Java
    It will be done,but now isn't done.

---
+++ GUI/Java_awt/Button.java
@@ -0,0 +1,9 @@
+package GUI.Java_awt;
+import java.awt.*;
+
+public class Button {
+    public static void main(String[] args){
+        Frame frame = new Frame("Button");
+        Button button = new Button();
+    }
+}
