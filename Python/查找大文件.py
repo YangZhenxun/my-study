b82e5b0fadbbb5ec645924d8e6825ce9c709562d
YangZhenxun <2210067895@qq.com>
2022-07-18 14:31:16 +0800

    It can find many big files.
    Its language is Chinese . It can find many big files.

--- Python/查找大文件.py
+++ Python/查找大文件.py
@@ -1,1 +1,22 @@
-import 
+from operator import length_hint
+import os
+from alive_progress import alive_bar
+
+def get_big_file(path,filesize):
+    with alive_bar(length_hint(os.walk(path))) as bar:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                target_file = os.path.join(dirpath, filename)
+                bar()
+                if not os.path.isfile(target_file):
+                    continue
+                size = os.path.getsize(target_file)
+                if size > filesize:
+                    size = size//(1024)
+                    size = '{size}KB'.format(size=size)
+                    print(target_file, size)
+
+if __name__ == '__main__':
+    a = input("请输入要查找的地址：")
+    b = int(input("请输入要查找的大小(KB)："))
+    get_big_file(a,b)
