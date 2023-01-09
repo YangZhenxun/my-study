321c9963a920c2c664bea8691914a9a27e33a72d
YangZhenxun <2210067895@qq.com>
2023-01-09 16:05:54 +0800

    Add new files.

---
+++ src.new/c++/additation/1/1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <random>
+
+int main()
+{
+    //创造随机数引擎
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution < > dis(1, 11);
+    while (true) {
+        //创造随机数
+        int i1 = dis(gen);
+        int i2 = dis(gen);
+        std::cout << i1 << "+" << i2 << "=";
+        int input;
+        std::cin >> input;
+        if (!std::cin.good()) {
+            std::cout << "请输入数字！" << std::endl;
+        }
+        else {
+            int math = i1 + i2;
+            if (input == math) {
+                std::cout << "你答对了！" << std::endl;
+            }
+            else {
+                std::cout << "你答错了！\n正确答案是：" << math << std::endl;
+            }
+        }
+        //清除数据
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
