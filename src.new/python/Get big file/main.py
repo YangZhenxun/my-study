5304b6c7ee4d8f77a910675bc03e2c07cfd65d09
YangZhenxun <2210067895@qq.com>
2023-08-07 10:34:22 +0800

    Update main.py

--- src.new/python/Get big file/main.py
+++ src.new/python/Get big file/main.py
@@ -9,6 +9,7 @@ import get_big_file_ui
 class AThread(QObject):
     signal_str = Signal(str)
     complete = Signal()
+    progress = Signal()
 
     def get_big_file(self, path, filesize):
         print("ok " + path + " " + str(filesize))
@@ -17,6 +18,7 @@ class AThread(QObject):
                 target_file = os.path.join(dirpath, filename)
                 if not os.path.isfile(target_file):
                     continue
+                self.progress.emit()
                 size = os.path.getsize(target_file)
                 size = size//1024
                 if size >= filesize:
@@ -31,15 +33,13 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        try:
-            self.ui = get_big_file_ui.Ui_MainWindow()
-            self.ui.setupUi(self)
-        except Exception as e:
-            print(e)
+        self.ui = get_big_file_ui.Ui_MainWindow()
+        self.ui.setupUi(self)
         self.work_athread = QThread()
         self.a_thread = AThread()
         self.a_thread.complete.connect(self.complete)
         self.a_thread.signal_str.connect(self.add_item)
+        self.a_thread.progress.connect(self.update_progress)
         self.a.connect(self.a_thread.get_big_file)
         self.a_thread.moveToThread(self.work_athread)
 
@@ -48,14 +48,19 @@ class MainWindow(QMainWindow):
 
     def complete(self):
         self.ui.pushButton.setEnabled(True)
+        self.ui.progressBar.setValue(100)
         self.work_athread.quit()
         self.work_athread.wait()
 
     def to_work_athread(self):
         self.ui.pushButton.setEnabled(False)
         self.ui.listWidget.clear()
+        self.ui.progressBar.setRange(0, 0)
         self.a.emit(self.ui.comboBox.currentText(), int(self.ui.lineEdit.text()))
         self.work_athread.start()
+
+    def update_progress(self):
+        self.ui.progressBar.setValue(int(self.ui.progressBar.value() + 1))
 
 
 app = QApplication(sys.argv)
