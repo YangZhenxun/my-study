9840ba99c7845a14ed231085cce91540f728ed36
杨震勋 <2210067895@qq.com>
2024-08-01 21:23:44 +0800

    Update lin.rs

--- src.new/rust/downloader/src/lin.rs
+++ src.new/rust/downloader/src/lin.rs
@@ -1,7 +1,7 @@
 pub fn lin<T>(i: T, list: Vec<T>) -> Option<T> where T: std::any::Any+PartialEq{
     for l in list.iter() {
         if &i == l {
-            return Option::Some(&i);
+            return Option::Some(i);
         }
     }
     return None;
